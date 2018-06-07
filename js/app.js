666796bedc9d26ffc96d03ff8d62728476da4126
Ana H Matrai <ahmatrai@gmail.com>
2018-06-06 23:10:28 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var function isValidCard() {
+  var sentence = prompt("Insira o número do cartão");
+  var element = document.getElementById("resultado");
+
+  if (sentence !== "") {
+  }
+
+  element.innerHTML = "Não foi inserido um número válido"
+  return false;
+}
